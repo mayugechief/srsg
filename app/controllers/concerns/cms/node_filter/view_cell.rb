f835c0f1eb540db20950b02de9ba796d70a4f277
tany <tanizawa@web-tips.co.jp>
2014-04-03 15:16:17 +0900

    fix: prepend_current_view_path for cells

--- app/controllers/concerns/cms/node_filter/view_cell.rb
+++ app/controllers/concerns/cms/node_filter/view_cell.rb
@@ -5,10 +5,15 @@ module Cms::NodeFilter::ViewCell
   
   included do
     helper ApplicationHelper
+    before_action :prepend_current_view_path
     before_action :inherit_variables
   end
   
   private
+    def prepend_current_view_path
+      prepend_view_path "app/cells/#{controller_path}"
+    end
+    
     def inherit_variables
       controller.instance_variables.select {|m| m =~ /^@[a-z]/ }.each do |name|
         instance_variable_set name, controller.instance_variable_get(name)
