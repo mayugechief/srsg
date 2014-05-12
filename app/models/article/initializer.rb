e2df2d74c5b7301ca32391546a7484773067d6fc
tany <una@kimiyure.com>
2014-05-13 01:08:47 +0900

    fix: inherit addons

--- app/models/article/initializer.rb
+++ app/models/article/initializer.rb
@@ -5,3 +5,15 @@ module Article
     Cms::Part.plugin "article/page"
   end
 end
+
+class Cms::Page
+  _addons = Article::Page.addons.map {|m| m.klass }
+  addons.each {|addon| Article::Page.include(addon.klass) unless _addons.include?(addon.klass) }
+  
+  class << self
+    def addon(*args)
+      Article::Page.addon *args
+      super
+    end
+  end
+end
--- app/models/cms/page.rb
+++ app/models/cms/page.rb
@@ -7,12 +7,4 @@ class Cms::Page
   include Model
   
   #default_scope ->{ where(route: "cms/page") }
-  
-  class << self
-    public
-      def addon(path)
-        Cms::Page::Model.include path.sub("/", "/addon/").camelize.constantize
-        super
-      end
-  end
 end
