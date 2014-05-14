44640a3148a54a6307a8e4a41843c9dd8551163c
tany <una@kimiyure.com>
2014-05-14 14:19:07 +0900

    add config: environment.yml

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,11 @@
+# Set up default environment.
+begin
+  require "yaml"
+  default_env = YAML.load_file File.expand_path('../../config/environment.yml', __FILE__)
+  ENV['RAILS_ENV'] ||= default_env["RAILS_ENV"]
+rescue LoadError
+end
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
