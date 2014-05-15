3d45050e6e682a86941f71afd57af188a2452463
tany <tanizawa@web-tips.co.jp>
2014-05-15 14:15:16 +0900

    fix: boot.rb

--- config/boot.rb
+++ config/boot.rb
@@ -1,10 +1,8 @@
 # Set up default environment.
-begin
-  require "yaml"
-  default_env = YAML.load_file File.expand_path('../../config/environment.yml', __FILE__)
-  ENV['RAILS_ENV'] ||= default_env["RAILS_ENV"]
-rescue LoadError
-end
+require "yaml"
+file = File.expand_path('../../config/environment.yml', __FILE__)
+env = File::exist?(file) ? YAML.load_file(file) : {}
+ENV['RAILS_ENV'] ||= env["RAILS_ENV"]
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
